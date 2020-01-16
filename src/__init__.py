1290e7476e5136bf96f85c3785d4aba4594fb83c
Chris McGuire <chris.mcguire@vistarmedia.com>
2020-01-16 14:23:21 -0500

    futurized

--- src/__init__.py
+++ src/__init__.py
@@ -1,9 +1,13 @@
-import ConfigParser
+from future import standard_library
+standard_library.install_aliases()
+from builtins import next
+from builtins import object
+import configparser
 
 class Upper(object):
     def __init__(self, iterable):
         self._iter = iter(iterable)
-    def next(self):
+    def __next__(self):
         return next(self._iter).upper()
     def __iter__(self):
         return self
